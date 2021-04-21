7609e617ba53fce4377cb03947e6177760ffe5bc
KIPKIPS <2432719656@qq.com>
2021-04-22 02:16:31 +0800

    添加mono单例和普通单例

---
+++ BeingAlive/Assets/Scripts/Base/MonoSingleton.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonoSingleton<T> : MonoBehaviour
+    where T : Component {
+    private static T _instance;
+
+    /// <summary>
+    /// 单例静态对象
+    /// </summary>
+    /// <value></value>
+    public static T Instance {
+        get {
+            if (_instance == null) {
+                _instance = FindObjectOfType(typeof(T)) as T;
+                if (_instance == null) {
+                    GameObject obj = new GameObject();
+                    obj.hideFlags = HideFlags.HideAndDontSave;
+                    _instance = (T)obj.AddComponent(typeof(T));
+                }
+            }
+            return _instance;
+        }
+    }
+    public virtual void Awake() {
+        DontDestroyOnLoad(this.gameObject);
+        if (_instance == null) {
+            _instance = this as T;
+        } else {
+            Destroy(gameObject);
+        }
+    }
+    public virtual void Start() {
+
+    }
+
+    // Update is called once per frame
+    public virtual void Update() {
+    }
+}
---
+++ BeingAlive/Assets/Scripts/Base/Singleton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> where T : class, new() {
+    private static T _instance;
+
+    /// <summary>
+    /// 单例静态对象
+    /// </summary>
+    /// <value></value>
+    public static T Instance {
+        get {
+            if (_instance == null) {
+                _instance = new T();
+            }
+            return _instance;
+        }
+    }
+}
